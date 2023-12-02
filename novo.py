c3fa406c3cfdcbcdbbafbe7c4e9cfd02558dd68e
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2023-12-02 16:29:10 -0300

    Add files via upload

---
+++ novo.py
@@ -0,0 +1,22 @@
+import pandas
+import requests
+from bs4 import BeautifulSoup
+
+html = ''
+url = 'http://brickset.com/sets/year-2023'
+
+req = requests.get(url)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+
+kits_list = []
+for item in soup.find_all('article', attrs={'class': 'set'}):
+    details = {
+        'title': item.find('h1').text,
+        'imagem' : item.find('img')['src'],
+        'partial_link': item.find('div' , attrs={'class' : 'meta'}).find('a')['href']
+    }
+    kits_list.append(details)
+
+df_kits = pandas.DataFrame.from_dict(kits_list)
+df_kits.to_excel('./dados.xlsx')
